86693887d41ef8b71bac8f340a2cfc48e2dcd888
DESKTOP-CKBSVRP\Lena <puchochek@gmail.com>
2018-03-19 20:44:27 +0300

    Add input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+// wwordsServer = 'https://wwords-server.herokuapp.com/translate?word=word';
+wwordsServer = 'http://localhost:3000/translate?word=word';
+  translate = 'any';
+  constructor(private httpClient: HttpClient){}
+  onKey(event: any) { // without type info
+    this.httpClient.get(this.wwordsServer)
+      .subscribe(res => {
+        console.log(res)
+      });
+    this.translate = event.target.value;
+  }
 }
