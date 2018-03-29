bfd5012f84b08b9b70b4d3f23563d38b60d7eb48
DESKTOP-CKBSVRP\Lena <puchochek@gmail.com>
2018-03-29 16:41:06 +0300

    *ngIf and *ngFor added

--- src/app/translation/translation.component.ts
+++ src/app/translation/translation.component.ts
@@ -13,8 +13,10 @@ export class TranslationComponent implements OnInit {
   // wwordsServer = 'http://localhost:3000/translate?word=';
   translate: string = '';
   timeId: any;
-
+  arr: any;
+  loading: boolean = false;
   constructor(private httpClient: HttpClient) {
+
     this.httpClient.get(this.wwordsServer + 'wake up', { withCredentials: true })
       .subscribe();
   }
@@ -23,20 +25,30 @@ export class TranslationComponent implements OnInit {
   }
 
   onKey(event: any) {
+  
+
     clearTimeout(this.timeId);
+  
     if (event.target.value) {
+
       this.timeId = setTimeout(() => {
         this.getTranslate(event.target.value);
       }, 1000);
+     
     } else {
       this.translate = '';
     }
   }
 
   getTranslate(word) {
+    this.loading = true;
     this.httpClient.get(this.wwordsServer + word, { withCredentials: true })
+
       .subscribe((res: any) => {
+       
+       
         this.translate = res.words;
+        this.loading = false;
       }, err => {
         console.log('err: ', err);
       });
